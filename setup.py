cb63ef82e6bec3380dd8acb4de15c5a3f6a3359a
Sharpz7 <adam.mcarthur62@gmail.com>
2025-01-14 09:42:21 +0000

    Fixed bugs

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(
-    name="stem-cv",
-    version="0.1.0",
-    packages=["stem_cv"],
-)
+setup(name="stem-cv", version="0.1.0", packages=find_packages())
